70f5ae38d34ef660dd31e542a3ee504f90ef59e8
karim arkia <karimarkia@gmail.com>
2019-11-28 08:47:03 +0200

    deleted hellow world

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    meals: [],
+    currMeal: null,
+  },
+  getters:{
+    meals(state) {
+
+      return state.meals
+    },
   },
   mutations: {
+    setToys(state, {toys}) {
+      state.toys = toys;
+      console.log('this :',state.toys);
+  },
   },
   actions: {
   },
