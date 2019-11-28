50421bed29edacb871757125959bbcb5ee468a0b
amit <abelamit8@gmail.com>
2019-11-28 09:53:30 +0200

    sss

--- src/views/MealEdit.vue
+++ src/views/MealEdit.vue
@@ -1,9 +1,9 @@
  <template> 
     <section >
-        <div class="edit-container">
+        <div class="edit-container" v-if="meal">
             <h2>Detailes</h2>
             <div class="inputs-container">
-                <span>Title</span> <input type="text" />
+                <span>Title</span> <input type="text" v-model="meal.title" />
                 <span>Country</span> <input type="text" />
                 <span>City</span> <input type="text" />
                 <span>Price</span> <input type="number" />
@@ -40,43 +40,43 @@
 <script>
 export default {
 
-    // data(){
-    //     return {
-    //         meal:{}           
-    //     }ghjmnfd
-    // },
+    data(){
+        return {
+            meal:{}           
+        }
+    },
     // methods:{
     //     save(){
-    //         if(this.meal._id){
+    //         if(this.meal.id){
     //             let meal = this.meal
-    //             this.$store.dispatch({type:'saveToy',meal})
+    //             this.$store.dispatch({type:'saveMeal',meal})
     //             .then(()=>this.$router.push(`/meal`))
     //         }
     //         else {
     //             let toy = JSON.parse(JSON.stringify(this.meal))
-    //             this.$store.dispatch({type:'addToy', meal})
+    //             this.$store.dispatch({type:'addMeal', meal})
     //             .then(()=>this.$router.push(`/meal`))
     //         }
             
     //     },
     // }, 
-    // computed:{
-    //     currMeal(){
-    //         return this.$store.getters.getCurrmeal
-    //     }
-    // },
-    //  created(){
-    //     let mealId = this.$route.params._id;
-    //     if(mealId){ 
-    //         // this.$store.dispatch({type:'getToys'})
-    //          this.$store.dispatch({type: "setCurrToy", mealId})
-    //             .then(() => {
-    //                  this.meal = JSON.parse(JSON.stringify(this.currMeal)); 
-    //             })
-
-    //     }
-
-    // } 
+    computed:{
+        currMeal(){
+            return this.$store.getters.currMeal;
+        }
+    },
+     created(){
+        let mealId = this.$route.params.id;
+        if(mealId){ 
+            // this.$store.dispatch({type:'getToys'})
+             this.$store.dispatch({type: "setCurrMeal", mealId})
+                .then(() => {
+                    console.log('fffff ',this.currMeal)
+                    this.meal = JSON.parse(JSON.stringify(this.currMeal)); 
+                    console.log('xxxx ',this.meal)
+                })
+        }
+    } 
     
 }
 </script>
