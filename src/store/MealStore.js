a5a45b0732637a402b014aeebe018036e0ccb498
amit <abelamit8@gmail.com>
2019-11-28 13:49:44 +0200

    edit wirks with no error

--- src/store/MealStore.js
+++ src/store/MealStore.js
@@ -30,9 +30,9 @@ export default ({
             state.meals.push(addedTodo)
 
         },
-        editMeal(state, { item }) {
-            const idx = state.meals.findIndex(currItem => currItem.id === item.id);
-            state.meals.splice(idx, 1, item);
+        editMeal(state,  {updatedMeal} ) {
+            const idx = state.meals.findIndex(currMeal => currMeal.id === updatedMeal.id);
+            state.meals.splice(idx, 1, updatedMeal.data);
         },
     },
 
