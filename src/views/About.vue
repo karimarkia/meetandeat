19fbe694de42fad297bf48e1164014905d0a8867
karim arkia <karimarkia@gmail.com>
2019-12-04 14:35:57 +0200

    changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
- <section>
-   
- </section>
+  <section class="about">
+    <h1>Bringing people together through food</h1>
+    <p>Meet&Eat is the world's largest community for authentic culinary experiences with locals, available in over 130 countries. We connect people who are seeking unique and immersive experiences with our hand-selected local hosts, in private homes and exclusive venues. With Meet&Eat, you're invited to pull up a seat at dinner party in an elegant Paris home, enjoy an Italian feast with a Roman family, take a cooking class in Tokyo, or marvel at the Manhattan skyline over a rooftop brunch. From home-cooks and food lovers to MasterChefs and Michelin-starred chefs, our hosts all share one special ingredient: a passion for bringing people together through food.</p>
+  </section>
 </template>
+
+<style>
+.about {
+  width: 70%;
+  margin: 0 auto;
+}
+.about h1 {
+  text-align: center;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  padding: 20px 0;
+}
+
+.about p{
+  font-size: 1.2rem;
+  line-height: 2;
+}
+
+</style>
