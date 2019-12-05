226240de41fb99fa6fb366bcc9d2ccfb035b1f8b
Artyom Pogosov <artyomp@outlook.com>
2019-12-05 11:00:44 +0200

    new commit

--- src/components/MealPreview.vue
+++ src/components/MealPreview.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="meal-card" v-if="meal">
      <router-link class="routerToDetails" :to="'/details/' + meal._id">
-    <img class="img-card img-card2" v-if="meal.imgUrl" :src="(meal.imgUrl[0])">
+    <img class="img-card img-card2 ratio-16-9" v-if="meal.imgUrl" :src="(meal.imgUrl[0])">
     <div class="cardTitle">
-    <h4>{{meal.location.city}}</h4>
+    <h4>Dinner in {{meal.location.city}} <span>{{mealDate}}</span></h4> 
      <h4><i class="fa fa-star"></i> {{meal.rate}}</h4>
       </div>
     <div class="test">
     <h4>{{meal.title}}</h4>
    
     </div>
-   <span class="price">{{meal.price}}$</span>
-   <span>{{mealDate}}</span>
+   <span class="price">{{meal.price}}$<span>/Per guest</span></span>
+  
 </router-link>
 </section>
 </template>
@@ -29,8 +29,9 @@ export default {
       return `/edit/${this.meal._id}`;
     },
         mealDate() {
-      let date = new Date(+this.meal.atDate);
-      date=date.toLocaleDateString()
+      let date = new Date(+this.meal.atDate)+'';
+         date = date.substring(3, 10)
+      // date=date.toLocaleDateString()
       //  date = date.toGMTString();
       // let age = today - birthDate;
       // age = age / (60000 * 60 * 24 * 365);
