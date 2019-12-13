de987c78e3a5b05f271b70ed63b75a1177bb47a4
Artyom Pogosov <artyomp@outlook.com>
2019-12-13 14:18:25 +0200

    new commit

--- src/views/NavHeader.vue
+++ src/views/NavHeader.vue
@@ -5,11 +5,11 @@
         <router-link to="/">
           <img src="@/img/logo.png" alt="Omnifood logo" class="logo-black-sec" />
         </router-link>
-         <form class="search" @submit.prevent="getFilter">
+         <form    v-if="(header==='/meal' )" class="search" @submit.prevent="getFilter">
                             <svg class="searchIcon" viewBox="0 0 16 16" role="presentation" aria-hidden="true" focusable="false" style="height: 18px; width: 18px; display: block; fill: currentcolor;"><path d="m2.5 7c0-2.5 2-4.5 4.5-4.5s4.5 2 4.5 4.5-2 4.5-4.5 4.5-4.5-2-4.5-4.5m13.1 6.9-2.8-2.9c.7-1.1 1.2-2.5 1.2-4 0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7c1.5 0 2.9-.5 4-1.2l2.9 2.8c.2.3.5.4.9.4.3 0 .6-.1.8-.4.5-.5.5-1.2 0-1.7"></path></svg>
 
         <el-input 
-          v-if="(header==='/meal' )"
+       
           class="searchCityNav"
           type="text"
    
